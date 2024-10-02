37d218a61d5cd2d752a6a2f8a1641ead8f31d542
leodoeio <basicleo787@gmail.com>
2024-10-02 07:45:42 +0530

    HIT-94:updated the env

--- src/shared/database/database.module.ts
+++ src/shared/database/database.module.ts
@@ -5,15 +5,19 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ProductPricing } from 'src/product-pricing/product-pricing.entity';
 import { Products } from 'src/product/product.entity';
 
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+
 @Module({
   imports: [
     TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'postgres',
+      type: (process.env.DB_TYPE as any) || 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT) || 5432,
+      database: process.env.DB_NAME || 'postgres',
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
       // entities: [__dirname + '/../**/*.entity{.ts,.js}'],
       entities: [Products, ProductPricing],
       synchronize: true,
