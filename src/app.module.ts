a307d2b788d2fada342d45d3db039bd95e45a7ef
leodoeio <basicleo787@gmail.com>
2024-10-03 13:52:03 +0530

    HIT-94:implented guard

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,8 @@ import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './shared/database/database.module';
 import { ProductsModule } from './product/product.module';
 import { ProductPricingModule } from './product-pricing/product-pricing.module';
+import { APP_GUARD } from '@nestjs/core';
+import { RolesGuard } from './auth/roles.guard';
 
 @Module({
   imports: [
@@ -20,6 +22,12 @@ import { ProductPricingModule } from './product-pricing/product-pricing.module';
     ProductsModule,
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: RolesGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -8,25 +8,32 @@ import {
   Delete,
   HttpException,
   HttpStatus,
+  UseGuards,
 } from '@nestjs/common';
 import { ProductsService } from './product.service';
 import { CreateProductDto, UpdateProductDto } from './product.dto';
+import { Roles } from '../auth/roles.decorator';
+import { RolesGuard } from '../auth/roles.guard';
 
 @Controller('products')
+@UseGuards(RolesGuard)
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
   @Post()
+  @Roles('admin')
   async createProduct(@Body() createProductDto: CreateProductDto) {
     return this.productsService.createProduct(createProductDto);
   }
 
   @Get()
+  @Roles('user', 'admin')
   async getProducts() {
     return this.productsService.getAllProducts();
   }
 
   @Get(':id')
+  @Roles('user', 'admin')
   async getProduct(@Param('id') id: string) {
     const product = await this.productsService.getProductById(id);
     if (!product) {
@@ -36,6 +43,7 @@ export class ProductsController {
   }
 
   @Put(':id')
+  @Roles('admin')
   async updateProduct(
     @Param('id') id: string,
     @Body() updateProductDto: UpdateProductDto,
@@ -44,6 +52,7 @@ export class ProductsController {
   }
 
   @Delete(':id')
+  @Roles('admin')
   async softDeleteProduct(@Param('id') id: string) {
     return this.productsService.softDeleteProduct(id);
   }
