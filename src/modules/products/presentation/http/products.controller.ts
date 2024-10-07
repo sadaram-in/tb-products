6a71dc07b9c404c57e600b18c0686296b130d759
leodoeio <basicleo787@gmail.com>
2024-10-07 08:50:55 +0530

    HIT-95:simple get method is implemented

--- src/modules/products/presentation/http/products.controller.ts
+++ src/modules/products/presentation/http/products.controller.ts
@@ -1,26 +1,42 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { ProductsService } from '../../application/products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { CreateProductCommand } from '../../application/commands/create-product.command';
 
-
 @Controller('products')
 export class ProductsController {
   constructor(private readonly productsService: ProductsService) {}
 
-  @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productsService.create(new CreateProductCommand(CreateProductDto.name));
-   
-  }
+  // @Post()
+  // create(@Body() createProductDto: CreateProductDto) {
+  //   return this.productsService.create(new CreateProductCommand(CreateProductDto.name));
+
+  // }
+
+  // @Get()
+  // findAll() {
+  //   //implement query command
+  //   return this.productsService.findAll();
+  // }
 
   @Get()
-  findAll() {
-    //implement query command
-    return this.productsService.findAll();
+  getLeo() {
+    return 'leo';
   }
 
+  @Post()
+  postLeo() {
+    return 'leo';
+  }
 
   //TODO add GET by id , PUT update and DELETE
 
