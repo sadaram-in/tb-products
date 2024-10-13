8e65e64dc1a5687e08fec667a8501ff885c69237
leodoeio <basicleo787@gmail.com>
2024-10-13 20:41:27 +0530

    HIT-95:simple test on app controller passed

--- src/app.controller.spec.ts
+++ src/app.controller.spec.ts
@@ -1,22 +1,48 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { CreateDummyDto } from './create-dummy.dto';
+import { ApiResponseDto } from './shared/response/dto/api-response.dto';
+import { ResponseService } from './shared/response/response.service';
 
 describe('AppController', () => {
   let appController: AppController;
+  let appService: AppService;
 
   beforeEach(async () => {
     const app: TestingModule = await Test.createTestingModule({
       controllers: [AppController],
-      providers: [AppService],
+      providers: [AppService,ResponseService],
     }).compile();
 
     appController = app.get<AppController>(AppController);
+    appService = app.get<AppService>(AppService);
   });
 
-  describe('root', () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe('Products API');
+  describe('getHello', () => {
+    it('should return the result from appService.getHello()', () => {
+      const result: ApiResponseDto<any> = {
+        payload: { data: 'Hello World!' },
+        status: 'success',
+        status_code: 200,
+        response_code: 200
+      };
+      jest.spyOn(appService, 'getHello').mockImplementation(() => result);
+
+      expect(appController.getHello()).toEqual(result);
+    });
+  });
+
+  describe('postHello', () => {
+    it('should return "hi"', () => {
+      const createDummyDto: CreateDummyDto = {
+        email: '',
+        password: '',
+      }; // Add properties as needed
+      const consoleSpy = jest.spyOn(console, 'log');
+
+      expect(appController.postHello(createDummyDto)).toBe('hi');
+      expect(consoleSpy).toHaveBeenCalledWith('Endpoint hit, returning hi');
     });
   });
 });
